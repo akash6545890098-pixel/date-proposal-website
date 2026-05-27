7567bffda683e73c314df185a87baabc220fc8e4
akash6545890098-pixel <akash6545890098@gmail.com>
2026-05-27 20:22:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
-function showPage(pageNumber) {
-  document.querySelectorAll('.page').forEach((page) => {
+function showPage(pageNumber){
+
+  document.querySelectorAll('.page').forEach((page)=>{
+
     page.classList.remove('active');
+
   });
 
-  document.getElementById(`page${pageNumber}`).classList.add('active');
+  document
+    .getElementById(`page${pageNumber}`)
+    .classList.add('active');
+
 }
+
+/* MOVING NO BUTTON */
 
 const noBtn = document.getElementById('noBtn');
 
-noBtn.addEventListener('mouseover', () => {
+noBtn.addEventListener('mouseover',()=>{
+
   const x = Math.random() * 300 - 150;
   const y = Math.random() * 300 - 150;
 
-  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+  noBtn.style.transform =
+  `translate(${x}px, ${y}px)`;
+
 });
+
+/* FOOD SELECT */
 
 const foods = document.querySelectorAll('.food');
 
-foods.forEach((food) => {
-  food.addEventListener('click', () => {
-    foods.forEach((f) => f.classList.remove('active-food'));
-    food.classList.add('active-food');
-    function sendWhatsApp() {
+foods.forEach((food)=>{
 
-  const selectedFood =
-    document.querySelector('.active-food span').innerText;
+  food.addEventListener('click',()=>{
+
+    foods.forEach((f)=>{
+
+      f.classList.remove('active-food');
+
+    });
+
+    food.classList.add('active-food');
+
+  });
+
+});
+
+/* WHATSAPP SEND */
+
+function sendWhatsApp(){
 
   const date =
-    document.querySelector('input[type=\"date\"]').value;
+  document.getElementById('dateInput').value;
 
   const time =
-    document.querySelector('input[type=\"time\"]').value;
+  document.getElementById('timeInput').value;
 
-  const address =
-    document.querySelector('textarea').value;
+  const location =
+  document.getElementById('locationInput').value;
+
+  const selectedFood =
+  document.querySelector('.active-food span')
+  .innerText;
 
   const message =
-`💖 Date Confirmed 💖
-
-🍴 Food: ${selectedFood}
+`💖 New Date Request 💖
 
 📅 Date: ${date}
 
 ⏰ Time: ${time}
 
-📍 Address: ${address}`;
+📍 Location: ${location}
+
+🍴 Food: ${selectedFood}`;
 
   const phoneNumber = "919064560840";
 
   const whatsappURL =
 `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
 
-  window.open(whatsappURL, '_blank');
-    }
-  });
-});
+  window.open(whatsappURL,'_blank');
+
+}
